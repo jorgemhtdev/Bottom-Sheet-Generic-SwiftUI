0e4a3fe7807478b58267388ccda796efa9b31bc5
jorgemhtdev <hola@jorgemht.dev>
2021-09-11 17:00:43 +0200

    Initial Commit

---
+++ Shared/BottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetApp.swift
+//  Shared
+//
+//  Created by jorgemht on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
